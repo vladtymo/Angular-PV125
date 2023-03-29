87e19870afabd618f2168d842c18acc9f0ece498
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-03-29 20:54:57 +0300

    Use input parameters.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
+import { MOVIES } from './../movies/movies-mock-data';
 import { Component } from '@angular/core';
+import { IMovie } from '../movies/movie';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-todo-app';
+
+  allMovies: IMovie[];
+  newMovies: IMovie[];
+
+  constructor() {
+    this.allMovies = MOVIES;
+    this.newMovies = MOVIES.filter(x => x.year >= 2018);
+  }
 }
--- src/app/components/movies/movie-list/movie-list.component.ts
+++ src/app/components/movies/movie-list/movie-list.component.ts
@@ -1,5 +1,5 @@
 import { IMovie } from '../movie';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Time } from '@angular/common';
 
 @Component({
@@ -9,7 +9,8 @@ import { Time } from '@angular/common';
 })
 export class MovieListComponent {
 
-  movies: IMovie[];
+  @Input()
+  movies: IMovie[] = [];
 
   movie: IMovie = {
     title: "",
@@ -18,12 +19,6 @@ export class MovieListComponent {
   };
 
   constructor() {
-    this.movies = [
-      { title: "Wrath of Man", genres: ["Action", "Crime"], year: 2021, duration: "1:58", isCurrent: false },
-      { title: "Sniper", genres: ["Action", "War"], year: 2022, duration: "1:40", isCurrent: true },
-      { title: "Fall", genres: ["Thriller", "Survival"], year: 2022, duration: "1:47", isCurrent: true },
-      { title: "The Fast and the Furious", genres: ["Crime", "Adventure"], year: 2001, duration: "1:46", isCurrent: false }
-    ];
   }
 
   addMovie(): void {
